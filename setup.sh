eded83f9b197835a69826299cd7f5b619551e17c
mantheys <sergio.manthey@ciemat.es>
2025-02-27 15:41:50 +0100

    UPDATE SETUP FILE TO PREVENT TOTAL CHAOS

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Print a warning in red color to only run this script from inside the scripts directory
-echo -e "\e[31mWARNING: Only run this script from main directory!\e[0m"
-read -p "Are you sure you want to continue? (y/n) " -n 1 -r
-
-# If the user did not answer with y, exit the script
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 1
-fi
+echo -e "\e[33mWARNING: Only run this script from main directory! (type 1 or 2)\e[0m"
+PS3="Are you sure you want to continue? "
+options=("Yes" "No")
+select opt in "${options[@]}"; do
+    case $opt in
+    "Yes")
+        break
+        ;;
+    "No")
+        # Exit script but do not exit terminal
+        return 0
+        ;;
+    *) echo "Invalid option" ;;
+    esac
+done
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/bin/geant4.sh
@@ -19,16 +27,30 @@ echo -e "\e[32mCreated directory for results\e[0m"
 
 # Check if directory data exists
 if [ ! -d "data" ]; then
-    mkdir data
-    sshfs ${USER}@gaeuidc1.ciemat.es:/pc/choozdsk01/DATA/CAJITA/output data
-    echo -e "\e[32mCreated directory for data\e[0m"
+    # If directory data does not exist, create symbolic link
+    ln -s /pc/choozdsk01/DATA/CAJITA/data $PWD
+    echo -e "\e[32mCreated symbolic link for data\e[0m"
+    echo -e "\e[32mSetup complete!\e[0m"
+
 else
-    # If directory data exists, check if it is empty
-    if [ -z "$(ls -A data)" ]; then
-        sshfs ${USER}@gaeuidc1.ciemat.es:/pc/choozdsk01/DATA/CAJITA/output data
-        echo -e "\e[32mCreated directory for data\e[0m"
-    else
-        echo -e "\e[32mData directory already exists and is not empty\e[0m"
-    fi
+    echo -e "\e[33mWARNING: Data folder exists. (type 1 or 2)\e[0m"
+    PS3="Do you want to delete it? "
+    options=("Yes" "No")
+    select opt in "${options[@]}"; do
+        case $opt in
+        "Yes")
+            rm -r data
+            echo -e "\e[31mDeleted data folder\e[0m"
+            break
+            ;;
+        "No")
+            break
+            ;;
+        *) echo "Invalid option" ;;
+        esac
+    done
 fi
+
+ln -s /pc/choozdsk01/DATA/CAJITA/data $PWD
+echo -e "\e[32mCreated symbolic link for data\e[0m"
 echo -e "\e[32mSetup complete!\e[0m"
